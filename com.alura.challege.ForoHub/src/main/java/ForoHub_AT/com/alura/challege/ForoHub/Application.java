a1e5247e9d3708c8fb2a89f4adc2e51118d83767
Angel <angeldtp03@hotmail.com>
2024-07-14 17:42:59 -0600

    Inicio

---
+++ com.alura.challege.ForoHub/src/main/java/ForoHub_AT/com/alura/challege/ForoHub/Application.java
@@ -0,0 +1,13 @@
+package ForoHub_AT.com.alura.challege.ForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ com.alura.challege.ForoHub/src/test/java/ForoHub_AT/com/alura/challege/ForoHub/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ForoHub_AT.com.alura.challege.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
